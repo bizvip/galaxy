dc11ed2177d703f65172290a6bd8fb98f52cec85
abudhabi <abudhabi@uaeabudhbai.com>
2023-02-18 00:17:48 +0800

    init

--- config/autoload/nacos.php
+++ config/autoload/nacos.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 return [
     // 'uri'=>'http://hosts.run',
     'host'     => '127.0.0.1',
-    'port'     => 8848,
+    'port'     => 8890,
     'username' => 'nacos',
     'password' => 'fucking123.',
     'guzzle'   => [
